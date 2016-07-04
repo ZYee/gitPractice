0a007d57ac5ed8acb569f73b6ed46003f2778431
ibstart <iosdevelop@ibstart.com>
2016-07-04 10:51:47 +0800

    Initial commit

---
+++ GitPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitPractice
+//
+//  Created by zhouyi on 7/4/16.
+//  Copyright © 2016 THQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitPractice
+//
+//  Created by zhouyi on 7/4/16.
+//  Copyright © 2016 THQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
